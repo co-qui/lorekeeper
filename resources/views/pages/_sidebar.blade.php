6e20072e8ee6b44692457eff3566a3aaad9c25ff
coquinuts <coquinuts@users.noreply.github.com>
2024-02-13 17:53:09 +0000

    refactor: fix blade formatting

--- resources/views/pages/_sidebar.blade.php
+++ resources/views/pages/_sidebar.blade.php
@@ -1,7 +1,7 @@
 <ul class="text-center">
     <li class="sidebar-header" style="margin-bottom: 1px;"><a href="#" class="card-link" style="font-size: 11px;">Featured Member</a></li>
     <li class="sidebar-section p-2">
-        @if(isset($featured_member) && $featured_member)
+        @if (isset($featured_member) && $featured_member)
             <div>
                 <a href="{{ $featured_member->url }}"><img src="{{ $featured_member->avatarUrl }}" class="img-thumbnail" /></a>
             </div>
