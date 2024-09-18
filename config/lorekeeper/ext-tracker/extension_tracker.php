9b12fe5d1449b8a1c0ab09d104e3f401b82a028b
coqui <help.kindlehearth@gmail.com>
2024-09-18 10:14:01 -0500

    ext tracker

--- config/lorekeeper/ext-tracker/extension_tracker.php
+++ config/lorekeeper/ext-tracker/extension_tracker.php
@@ -1,9 +1,24 @@
 <?php
 
 return [
-    'wiki_key' => 'Extension_Tracker',
-    'creators' => json_encode([
-        'Uri' => 'https://github.com/preimpression/',
-    ]),
-    'version'  => '1.4.0',
+    // FORMAT EXAMPLE.
+    //
+    // 'extension_tracker' => [
+    //     'key' => 'extension_tracker',
+    //     'wiki_key' => 'Extension_Tracker',
+    //     'creators' => json_encode([
+    //         'Uri' => 'https://github.com/preimpression/',
+    //     ]),
+    //     'version' => '1.0.0',
+    // ],
+
+    'staff_guide_pages' => [
+        'key' => 'staff_guide_pages',
+        'wiki_key' => 'Staff Guide Pages',
+        'creators' => json_encode([
+            'Cylunny' => 'https://toyhou.se/cylunny',
+            'coqui' => 'https://toyhou.se/co-qui', //All credit to Cyl! I just made it staff-onlly!
+        ]),
+        'version' => '1.0.0', 
+    ],
 ];
